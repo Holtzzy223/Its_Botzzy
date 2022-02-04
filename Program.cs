9ed3dcdb6a5cb0aff214e4c8fe69e357c9585e72
Holtzzy223 <70851222+Holtzzy223@users.noreply.github.com>
2022-02-03 22:11:58 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchTools;
+namespace TwitchBot
+{
+   class Program
+    {
+        // To Generate new token go here => https://twitchapps.com/tmi/
+        static bool startBot = false;
+        static TwitchChatHandle chatHandle;
+       
+        static void Main(string[] args)
+        {
+
+            chatHandle = new TwitchChatHandle(GetAuthToken(), GetStreamName());
+           
+            while (startBot)
+            {
+                var chatEntry = chatHandle.Read();
+               //log chat to console
+                Console.WriteLine($"{chatEntry.sender}:{chatEntry.message}");
+                
+                if (chatEntry.message.Contains('!'))
+                {
+                    ParseCommands(chatEntry);
+                }
+                Thread.Sleep(50);
+            }
+
+
+
+        }
+
+        private static string GetAuthToken()
+        {
+            string oAuth = "";
+            Console.WriteLine("Enter oAuth token filepath: ");
+            string authTokenPath = Console.ReadLine();
+            oAuth = File.ReadAllText(authTokenPath);
+            return oAuth;
+        }
+
+        static string GetStreamName()
+        {
+            Console.WriteLine("Enter stream name: ");
+            string streamName = Console.ReadLine();
+            if (streamName != null)
+            {
+                startBot = true;
+                return streamName;
+            }
+            else { return "" ; }
+            
+        }
+
+        
+        static void ParseCommands(ChatEntry chatEntry)
+        { 
+            string chatMessage = chatEntry.message;
+            var startIndex = chatMessage.IndexOf('!')+1;
+            string chatCommand = chatMessage.Substring(startIndex);
+            Console.WriteLine($"{"Command Issued: "}{chatEntry.sender}:{chatCommand}");
+            if (chatEntry.sender == "its_holtzzy") { chatHandle.Write("Yes Sir!"); }
+            switch (chatCommand)
+            {
+                case"cauldron":
+                    chatHandle.Write($"{"Creature Soup @"}{chatEntry.sender}");
+                    break;
+                case"hello":
+                    chatHandle.Write($"{"Hello there. @"}{chatEntry.sender}");
+                    break;
+                case"time":
+                    chatHandle.Write($"{DateTime.UtcNow}{" UTC @"}{chatEntry.sender}");
+                    break;
+                case "lurk":
+                    chatHandle.Write("OMG LOL OK");
+                    break;
+                case "itch":
+                    chatHandle.Write("https://its-holtzzy.itch.io/");
+                    break;
+                case"github":
+                    chatHandle.Write("https://github.com/holtzzy223");
+                    break;
+                case "bye":
+                    chatHandle.Write("Good riddance!");
+                    break;
+                case "nft":
+                    chatHandle.Write("Right click, save as...");
+                    break;
+                case "bot":
+                    chatHandle.Write("BEHOLD BOTZZY");
+                    break;
+                    
+            }
+            
+        }
+
+    }
+}
---
+++ Tools.cs
@@ -0,0 +1,119 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwitchTools
+{
+    class TwitchChatHandle
+    {
+  
+
+        //OAuth can be retrieved from https://twitchapps.com/tmi/
+
+        string channel;
+
+        TcpClient tcpClient;
+        StreamReader inputStream;
+        StreamWriter outputStream;
+
+        /// <summary>
+        /// Allows you to read and write Twitch chat messages.
+        /// </summary>
+        /// <param name="oauth">OAuth token.</param>
+        /// <param name="channel">Streamer's channel.</param>
+        public TwitchChatHandle(string oauth, string channel)
+        {
+            this.channel = channel;
+
+            tcpClient = new TcpClient("irc.twitch.tv", 6667);
+            inputStream = new StreamReader(tcpClient.GetStream());
+            outputStream = new StreamWriter(tcpClient.GetStream());
+
+            outputStream.WriteLine($"PASS {oauth}");
+            outputStream.WriteLine($"NICK bot");
+            outputStream.WriteLine($"USER bot 8 * :bot");
+            outputStream.WriteLine($"JOIN #{channel}");
+            outputStream.Flush();
+
+            Task.Run(() => Ping());
+        }
+
+        /// <summary>
+        /// Returns the next Twitch chat entry.
+        /// </summary>
+        /// <returns></returns>
+        public ChatEntry Read()
+        {
+            string chatEntry;
+        retry:
+            try
+            {
+                chatEntry = inputStream.ReadLine();
+            }
+            catch
+            {
+                goto retry;
+            }
+            if (chatEntry == null || !chatEntry.Contains("PRIVMSG")) goto retry;       //Not Chat Message
+
+            //Get sender's name
+            int startIndex = chatEntry.IndexOf(":") + 1;
+            int endIndex = chatEntry.IndexOf("!");
+            int length = endIndex - startIndex;
+            string chatSender = chatEntry.Substring(startIndex, length);
+
+            //Get chat message
+            startIndex = chatEntry.IndexOf(":", 1) + 1;
+            string chatMessage = chatEntry.Substring(startIndex);
+            return new ChatEntry(chatSender, chatMessage);
+        }
+
+        /// <summary>
+        /// Writes a message to chat.
+        /// </summary>
+        /// <param name="chatMessage">Message to write.</param>
+        public void Write(string chatMessage)
+        {
+            Send($":bot!bot@bot.tmi.twitch.tv PRIVMSG #{channel} :{chatMessage}");
+        }
+
+        void Send(string text)    //Sends data via TCP (Doesn't print to chat)
+        {
+        retry:
+            try
+            {
+                outputStream.WriteLine(text);
+                outputStream.Flush();
+            }
+            catch
+            {
+                goto retry;
+            }
+        }
+
+        void Ping()     //Pings server every 5 minutes to prevent disconnection
+        {
+            while (true)
+            {
+                Send("PING irc.twitch.tv");
+                Thread.Sleep(TimeSpan.FromMinutes(5));
+            }
+        }
+    }
+    struct ChatEntry
+    {
+        public string sender;
+        public string message;
+        public ChatEntry(string Sender, string Message)
+        {
+            sender = Sender;
+            message = Message;
+        }
+    }
+}
